37e40ed39d47be5ab5da92ecd5e6c08dcbf3df7f
Ryan Gelston <rgelston@calpoly.edu>
2018-12-07 21:53:32 -0800

    aifdsa

---
+++ plotData.py
@@ -0,0 +1,21 @@
+# Name: Ryan Gelston
+# Class: CSC 466
+# Assignment: Final Project
+# Description: Contains functions that plot various aspects of the dataset.
+
+import matplotlib as mpl
+
+def ratingsOverTime(data, file_name=None):
+   """Plots the ratings over time for the dataset
+
+      Parameters:
+         data -- List of ratings dictionaries (see sqlToVec.py)
+         file_name -- Write plot to file of the passed name. Outputs plot 
+            screen if None.
+
+      Returns:
+         Statistical information about the ratings over time
+   """
+
+
+
