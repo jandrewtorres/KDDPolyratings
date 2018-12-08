72b405b50b2c32bf3a2cfa6942e3ffe603d53d1a
Ryan Gelston <rgelston@calpoly.edu>
2018-12-07 18:53:54 -0800

    Added sqlToVec.py

---
+++ sqlToVec.py
@@ -0,0 +1,21 @@
+# Name: John Torres and Ryan Gelston
+# Class: CSC 466
+# Assignment: Term Project
+# Description: Reads ratings from sql database and turns each entry into a 
+#  dictionary object
+
+"""
+Data schema:
+
+{
+   "Date" : ,
+   "Required" : [True | False],
+   "Course" : , 
+   "Class" : "String",
+   "Lecturer" : "String",
+   "Grade" : ,
+   "Review" :
+}
+"""
+
+
